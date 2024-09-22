acb24a106c84870ce30f15c3a70e6febc862f899
Tomás Martín <tmartindsgn@gmail.com>
2024-09-22 10:55:33 +0200

    lab done

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4  + " " + s5 + " " +  s3 + " " + s2 + " " + s1 + " " + s4;
 
 // Print out the concatenated string
 
 
-
+console.log(tongueTwister)
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,11 +22,14 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const convertPart1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const convertPart2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
+const cameLtaiL = convertPart1 + convertPart2;
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(cameLtaiL);
 
 
 /*******************************************
@@ -35,11 +38,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
 
-
+console.log(tipAmount);
 
 
 /*******************************************
@@ -47,11 +50,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
 
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +77,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+// false
+// true
+// false
+// true
+// true
+// false
+// true
